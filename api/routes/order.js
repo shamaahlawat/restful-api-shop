0e8a16097cb8809d9d7d0bd9df7003084d5d6aaa
Shama Ahlawat <shama@oriserve.com>
2019-03-08 01:28:15 +0530

    error handling added

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//handle incoming GET request to /orders 
 router.get('/',(req,res,next)=>{
     res.status(200).json({
         message: 'orders were fetched'
--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require('express');
 
 const app = express();
+const morgan = require('morgan'); //to see console logs
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/order');
 
+app.use(morgan('dev')); //use it before routes
+
+//routes which should handle requests
 app.use('/products', productRoutes); // always connect your route here
 app.use('/order', orderRoutes); 
+
+
+//error handling 
+app.use((req,res,next) => {
+    const error = new Error('not found');
+    next(error);
+})
+
+app.use((error,req,res,next) => {
+ res.status(error.status || 500)
+ res.json({
+     error:{
+         message:error.message
+     }
+ })
+})
 
 // app.use((req,res,next)=>{
 //    res.status(200).json({
